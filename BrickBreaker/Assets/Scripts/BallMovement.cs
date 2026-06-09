02245e1a1962e17e55018b6f3febd359eb414d48
Keaton Caldwell <kcaldwell2@berklee.edu>
2026-06-09 13:48:56 -0400

    ball movement added

--- BrickBreaker/Assets/Scripts/BallMovement.cs
+++ BrickBreaker/Assets/Scripts/BallMovement.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    public float Speed = 7.0f;
+
+    private int _xDirection;
+    
+    private int _yDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _xDirection = Random.value < 0.5f ? 1 : -1;
+        _yDirection = Random.value < 0.5f ? 1 : -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(
+            Speed * _xDirection,
+            Speed * _yDirection,
+            0.0f
+        ) * Time.deltaTime);
     }
 }
